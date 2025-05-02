ea2b70f1182e3a76da529ff6a7f9548e6aaec783
Anthony Rocha <anthony.rocha@amd.com>
2025-05-02 00:37:36 -0700

    AST1060 Starter

---
+++ app/ast1060-starter/src/main.rs
@@ -0,0 +1,29 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#![no_std]
+#![no_main]
+
+// We have to do this if we don't otherwise use it to ensure its vector table
+// gets linked in.
+
+use cortex_m_rt::entry;
+use ast1060_pac::Peripherals;
+#[entry]
+fn main() -> ! {
+
+    // This code just forces the ast1060 pac to be linked in.
+    let peripherals = unsafe {
+        Peripherals::steal()
+    };
+    peripherals.scu.scu000().modify(|_, w| {
+        w
+    });
+
+
+    // Default boot speed, until we bother raising it:
+    const CYCLES_PER_MS: u32 = 16_000;
+
+    unsafe { kern::startup::start_kernel(CYCLES_PER_MS) }
+}
